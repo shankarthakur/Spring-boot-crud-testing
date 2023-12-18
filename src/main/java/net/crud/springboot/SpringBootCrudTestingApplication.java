0b4f7faae3134421c992fdd7fba1e0a0cbd7afbe
shankar thakur <shankarthaur07@gmail.com>
2023-12-18 17:35:38 +0530

    first commit

---
+++ src/main/java/net/crud/springboot/SpringBootCrudTestingApplication.java
@@ -0,0 +1,13 @@
+package net.crud.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/crud/springboot/SpringBootCrudTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package net.crud.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
